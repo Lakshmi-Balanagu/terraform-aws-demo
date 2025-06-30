08b647a05fc3ff9a1c3353d8727bc57efd392386
Lakshmi-Balanagu <lakshmi.balanagu12@gmail.com>
2025-06-30 20:57:00 +0530

    Add files via upload

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2"
+  type        = string
+}
